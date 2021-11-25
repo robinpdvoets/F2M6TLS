bb3d61e8493537ec5b6ef2fa8994be1ab9f55ec3
Raphael Rebel <you@example.comraphaelrebel@live.com>
2021-11-25 21:06:36 +0100

    Added new features

--- header.php
+++ header.php
@@ -6,7 +6,13 @@
 
 
 <body <?php body_class(); ?>>
-<div class="header">
-    <h1>Hello world!</h1>
+<div class="header ">
+    <h1>Welcome to <br/>
+        <?php bloginfo('name'); ?>
+    </h1>
+</div>
+
+<div class="main ">
+    <h2>This is the body!</h2>
 </div>
 </body>
